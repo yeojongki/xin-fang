80fad25f4ab6ccedd502d68b514c943da08a9055
yeojongki <kk597@sina.com>
2019-09-28 01:45:56 +0800

    fix: fetch api currentUser twice when init
    affects: @xf/admin, @xf/common, @xf/server

--- packages/admin/src/layouts/SecurityLayout.tsx
+++ packages/admin/src/layouts/SecurityLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import { Redirect } from 'umi';
+import { stringify } from 'querystring';
 import { ConnectState, ConnectProps } from '@/models/connect';
 import { CurrentUser } from '@/models/user';
 import PageLoading from '@/components/PageLoading';
@@ -29,12 +30,18 @@ class SecurityLayout extends React.Component<SecurityLayoutProps, SecurityLayout
   render() {
     const { isReady } = this.state;
     const { children, loading, currentUser } = this.props;
-    const token = getStorageToken();
-    if ((!currentUser.username && loading) || !isReady) {
+    // You can replace it to your authentication rule (such as check token exists)
+    // 你可以把它替换成你自己的登录认证规则（比如判断 token 是否存在）
+    const isLogin = (currentUser && currentUser.username) || getStorageToken();
+    const queryString = stringify({
+      redirect: window.location.href,
+    });
+
+    if ((!isLogin && loading) || !isReady) {
       return <PageLoading />;
     }
-    if (!token) {
-      return <Redirect to="/user/login" />;
+    if (!isLogin) {
+      return <Redirect to={`/user/login?${queryString}`} />;
     }
     return children;
   }
